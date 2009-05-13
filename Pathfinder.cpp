0a1726b3094e3b14108c0644dfa4b9f85f9eb0f6
Error323 <folkerthuizinga@gmail.com>
2009-05-13 21:47:28 +0200

    Pathfinder: Optimization

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -20,26 +20,25 @@ void CPathfinder::update(float *weights) {
 
 void CPathfinder::successors(ANode *an, std::queue<ANode*> &succ) {
 	Node *s, *n = dynamic_cast<Node*>(an);
-	int x,z;
-
-	for (int i = -1; i <= 1; i++) {
-		for (int j = -1; j <= 1; j++) {
-			/* Don't add the parent node */
-			if (i == 0 && j == 0) continue;
-			x = n->x+i; z = n->z+j;
-
-			/* Check we are within boundaries */
-			if (x < X && x >= 0 && z < Z && z >= 0) { 
-				s = &map[id(x, z)];
-				if (!s->blocked()) succ.push(s);
-			}
-		}
-	}
+	s = &map[id(n->x-1, n->z-1)]; succ.push(s); /* Top left  */
+	s = &map[id(n->x  , n->z-1)]; succ.push(s); /* Top mid   */
+	s = &map[id(n->x+1, n->z-1)]; succ.push(s); /* Top right */
+	s = &map[id(n->x-1, n->z  )]; succ.push(s); /* Mid left  */
+	s = &map[id(n->x+1, n->z  )]; succ.push(s); /* Mid right */
+	s = &map[id(n->x-1, n->z+1)]; succ.push(s); /* Bot left  */
+	s = &map[id(n->x  , n->z+1)]; succ.push(s); /* Bot mid   */
+	s = &map[id(n->x+1, n->z+1)]; succ.push(s); /* Bot right */
 }
 
 bool CPathfinder::path(float3 &s, float3 &g, std::vector<float3> &path) {
-	int ids = id(round(s.x/RES), round(s.z/RES));
-	int idg = id(round(g.x/RES), round(g.z/RES));
+	/* If exceeding, snap to boundaries */
+	int sx  = int(round(s.x/RES)); sx = std::max<int>(sx, 1); sx = std::min<int>(sx, X-2);
+	int sz  = int(round(s.z/RES)); sz = std::max<int>(sz, 1); sz = std::min<int>(sz, Z-2);
+	int gx  = int(round(g.x/RES)); gx = std::max<int>(gx, 1); gx = std::min<int>(gx, X-2);
+	int gz  = int(round(g.z/RES)); gz = std::max<int>(gz, 1); gz = std::min<int>(gz, Z-2);
+	
+	int ids = id(sx, sz);
+	int idg = id(gx, gz);
 	start = &map[ids];
 	goal = &map[idg];
 	std::vector<ANode*> nodepath;
