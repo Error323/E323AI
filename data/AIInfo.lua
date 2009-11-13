59eb270eb507d05354eb84022624ea5c8a121a49
Error323 <folkerthuizinga@gmail.com>
2009-11-13 23:30:53 +0100

    Version bump

--- data/AIInfo.lua
+++ data/AIInfo.lua
@@ -21,7 +21,7 @@ local infos = {
 	},
 	{
 		key    = 'version',
-		value  = '3.8.2', -- AI version - !This comment is used for parsing!
+		value  = '3.10.1', -- AI version - !This comment is used for parsing!
 	},
 	{
 		key    = 'name',
