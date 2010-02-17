521b4e8601fc1f51ed6ca0a97d78af8530cffc97
slogic <slspam@land.ru>
2010-02-18 00:07:40 +0300

    Added a switch to select between SDL_GetTicks and clock() in scoped timers

--- CScopedTimer.h
+++ CScopedTimer.h
@@ -7,7 +7,13 @@
 #include <map>
 #include <limits>
 
-#include <SDL_timer.h>
+#define USE_SDL_TIMER
+
+#ifdef USE_SDL_TIMER
+	#include <SDL_timer.h>
+#else
+	#include <time.h>
+#endif
 
 #define MAX_STR_LENGTH 30
 
@@ -25,19 +31,27 @@ class CScopedTimer {
 				times[task] = 0;
 				counters[task] = 0;
 				maxtimes[task] = 0;
-				mintimes[task] = std::numeric_limits<int>::max();
+				mintimes[task] = std::numeric_limits<unsigned>::max();
 			}
+#ifdef USE_SDL_TIMER
 			t1 = SDL_GetTicks();
+#else
+			t1 = clock();
+#endif
 		}
 
 		~CScopedTimer() {
-			t2           = SDL_GetTicks();
-			t3           = t2 - t1;
+#ifdef USE_SDL_TIMER
+			t2 = SDL_GetTicks();
+#else
+			t2 = clock();
+#endif
+			t3 = t2 - t1;
 			times[task] += t3;
 			if (t3 > 0) // we are not interested in 0 timings
 				mintimes[task] = std::min<unsigned>(mintimes[task], t3);
 			maxtimes[task] = std::max<unsigned>(maxtimes[task], t3);
-			sum         += t3;
+			sum += t3;
 			counters[task]++;
 		}
 
@@ -56,7 +70,14 @@ class CScopedTimer {
 				float avg = x / 100.0f;
 				ss << "  " << i->first;
 				for (unsigned j = i->first.size()+2; j < MAX_STR_LENGTH; j++)
-				  ss << " ";
+					ss << " ";
+#ifndef USE_SDL_TIMER
+				if(mintimes[i->first] == std::numeric_limits<unsigned>::max())
+					mintimes[i->first] = 0;
+				mintimes[i->first] = unsigned(((float)mintimes[i->first] / CLOCKS_PER_SEC) * 1000.0f);
+				maxtimes[i->first] = unsigned(((float)maxtimes[i->first] / CLOCKS_PER_SEC) * 1000.0f);
+				i->second = unsigned(((float)i->second / CLOCKS_PER_SEC) * 1000.0f);
+#endif
 				ss << pct << "%\t" << avg << "\t" << mintimes[i->first] << "\t" << maxtimes[i->first] << "\t" << i->second << "\t" << "\n";
 			}
 			ss << "\n";
@@ -71,7 +92,7 @@ class CScopedTimer {
 		unsigned t2;
 		unsigned t3;
 
-		static unsigned sum;
+		static unsigned sum; // total time for all timers
 		static std::map<std::string, unsigned> times;
 		static std::map<std::string, unsigned> counters;
 		static std::map<std::string, unsigned> mintimes,maxtimes;
