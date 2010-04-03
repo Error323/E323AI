3b41700d916c6591a9e0143769490453a753d15e
Error323 <folkerthuizinga@gmail.com>
2010-04-03 20:10:35 +0200

    Disable crosscompiling since it doesn't work

--- releases/release.sh
+++ releases/release.sh
@@ -9,9 +9,9 @@ rm -rf AI/Skirmish/E323AI
 make -j5 E323AI
 
 # windows compile
-cd ~/workspace/spring/win
-rm -rf AI/Skirmish/E323AI
-make -j5 E323AI
+# cd ~/workspace/spring/win
+# rm -rf AI/Skirmish/E323AI
+# make -j5 E323AI
 
 cd ${CURDIR}
 
@@ -23,8 +23,8 @@ mkdir -vp ${DIR}
 
 cp -vr ../data/* ${DIR}
 cp -vr ../../../../linux/AI/Skirmish/E323AI/libSkirmishAI.so ${DIR}
-cp -vr ../../../../win/AI/Skirmish/E323AI/SkirmishAI.dll ${DIR}
+# cp -vr ../../../../win/AI/Skirmish/E323AI/SkirmishAI.dll ${DIR}
 
-tar -cvzf E323AI-v${VERSION}-HighTemplar.tgz AI
-
-rm -rfv AI
+# tar -cvzf E323AI-v${VERSION}-HighTemplar.tgz AI
+# 
+# rm -rfv AI
