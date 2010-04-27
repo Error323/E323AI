ee8dc48a4c3eae9bf7264883f3fa40b01e74384c
hoijui <hoijui.quaero@gmail.com>
2010-04-27 10:39:59 +0200

    adjust to spring master, commit: b4aa80a40ee5e30c6a9e79cbd595882b131d6745 This breaks compatibility with spring release version 0.81(.2)

--- headers/HAIInterface.h
+++ headers/HAIInterface.h
@@ -2,11 +2,11 @@
 #define HAIINTERFACE_H
 
 /* Spring AI Interface Headers */
-#include "ExternalAI/aibase.h"                 /* DLL exports and definitions */
-#include "ExternalAI/IGlobalAI.h"              /* Main AI file */
-#include "ExternalAI/IAICallback.h"            /* Callback functions */
-#include "ExternalAI/IGlobalAICallback.h"      /* AI Interface */
-#include "ExternalAI/IAICheats.h"              /* AI Cheat Interface */
+#include "LegacyCpp/aibase.h"                  /* DLL exports and definitions */
+#include "LegacyCpp/IGlobalAI.h"               /* Main AI file */
+#include "LegacyCpp/IAICallback.h"             /* Callback functions */
+#include "LegacyCpp/IGlobalAICallback.h"       /* AI Interface */
+#include "LegacyCpp/IAICheats.h"               /* AI Cheat Interface */
 #include "ExternalAI/Interface/aidefines.h"    /* SNPRINTF, STRCPY, ... */
 
 #endif
