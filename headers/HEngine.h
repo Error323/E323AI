82657722ece554eb6f6e564bdf9e71c10d10989c
hoijui <hoijui.quaero@gmail.com>
2010-12-07 17:59:22 +0100

    adjust to Legacy C++ Wrapper isolation [AI]

--- headers/HEngine.h
+++ headers/HEngine.h
@@ -2,11 +2,11 @@
 #define HENGINE_H
 
 /* Spring Engine Headers */
-#include "LegacyCpp/UnitDef.h"                 /* Unit Definitions */
-#include "Sim/Units/CommandAI/CommandQueue.h"  /* Unit Command Queues */
-#include "Sim/Features/FeatureDef.h"           /* Feature Definitions */
-#include "Sim/MoveTypes/MoveInfo.h"            /* Types of Movement units can have */
-#include "Sim/Weapons/WeaponDefHandler.h"      /* Weapon Definitions */
-#include "System/float3.h"                     /* 3D vector operations */
+#include "LegacyCpp/UnitDef.h"       /* Unit Definitions */
+#include "LegacyCpp/CommandQueue.h"  /* Unit Command Queues */
+#include "LegacyCpp/FeatureDef.h"    /* Feature Definitions */
+#include "LegacyCpp/MoveData.h"      /* Types of Movement units can have */
+#include "LegacyCpp/WeaponDef.h"     /* Weapon Definitions */
+#include "System/float3.h"           /* 3D vector operations */
 
 #endif 
