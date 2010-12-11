3cce1fca4cbbbe873c501830cbfc9d6fe2c46dde
slogic <slspam@land.ru>
2010-12-11 22:42:09 +0300

    CAI refactor

--- CAI.cpp
+++ CAI.cpp
@@ -2,16 +2,41 @@
 
 #include "../AI/Wrappers/LegacyCpp/AIGlobalAI.h"
 
-extern std::map<int, CAIGlobalAI*> myAIs;
 
 std::vector<int> AIClasses::unitIDs;
+std::map<int, AIClasses*> AIClasses::instances;
+std::map<int, std::map<int, AIClasses*> > AIClasses::instancesByAllyTeam;
 
-AIClasses::AIClasses() {
-	difficulty = DIFFICULTY_HARD;
+AIClasses::AIClasses(IGlobalAICallback* callback):difficulty(DIFFICULTY_HARD) {
 	if (unitIDs.empty())
 		unitIDs.resize(MAX_UNITS);
+	
+	cb = callback->GetAICallback();
+	cbc = callback->GetCheatInterface();
+	
+	cbc->EnableCheatEvents(true);
+
+	team = cb->GetMyTeam();
+	allyTeam = cb->GetMyAllyTeam();
+
+	updateAllyIndex();
+
+	instances[team] = this;
+	instancesByAllyTeam[allyTeam][team] = this;
 }
 
-bool AIClasses::isMaster() {
-	return (myAIs.begin()->first == team);
+AIClasses::~AIClasses() {
+	instances.erase(team);
+}
+
+void AIClasses::updateAllyIndex() {
+	allyIndex = 1;
+
+	std::map<int, AIClasses*>::iterator it;
+	for (it = instances.begin(); it != instances.end(); ++it) {
+		if (it->first != team) {
+			if (it->second->allyTeam == allyTeam)
+				allyIndex++;
+		}
+	}
 }
