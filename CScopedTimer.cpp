72365e5d043e2f1a86cf72e870c7214f78c1fcfa
slogic <slspam@land.ru>
2010-12-11 22:54:07 +0300

    Code reformatting

--- CScopedTimer.cpp
+++ CScopedTimer.cpp
@@ -1,6 +1,47 @@
 #include "CScopedTimer.h"
 
+
 std::map<std::string, int> CScopedTimer::taskIDs;
 std::map<std::string, unsigned int> CScopedTimer::curTime;
 std::map<std::string, unsigned int> CScopedTimer::prevTime;
 std::vector<std::string> CScopedTimer::tasks;
+
+CScopedTimer::CScopedTimer(const std::string& s, IAICallback *_cb): cb(_cb), task(s) {
+	initialized = true;
+	
+	if (std::find(tasks.begin(), tasks.end(), task) == tasks.end()) {
+		taskIDs[task] = tasks.size();
+#if !defined(BUILDING_AI_FOR_SPRING_0_81_2)
+		cb->DebugDrawerSetGraphLineColor(taskIDs[task], colors[taskIDs[task] % 8]);
+		cb->DebugDrawerSetGraphLineLabel(taskIDs[task], task.c_str());
+#endif
+		tasks.push_back(task);
+		
+		curTime[task] = cb->GetCurrentFrame();
+		prevTime[task] = 0;
+	}
+
+	t1 = GetEngineRuntimeMSec();
+}
+
+CScopedTimer::~CScopedTimer() {
+	t2 = GetEngineRuntimeMSec();
+
+	if (!initialized)
+		return;
+#if !defined(BUILDING_AI_FOR_SPRING_0_81_2)
+		unsigned int curFrame = cb->GetCurrentFrame();
+			
+	for (size_t i = 0; i < tasks.size(); i++) {
+		const int taskID = taskIDs[tasks[i]];
+		
+		if (tasks[i] == task)
+			prevTime[task] = t2 - t1;
+		
+		cb->DebugDrawerAddGraphPoint(taskID, curFrame, prevTime[tasks[i]]);
+
+		if ((curFrame - curTime[tasks[i]]) >= TIME_INTERVAL)
+			cb->DebugDrawerDelGraphPoints(taskID, 1);
+	}
+#endif
+}
--- CScopedTimer.h
+++ CScopedTimer.h
@@ -1,5 +1,5 @@
-#ifndef SCOPEDTIMER_HDR
-#define SCOPEDTIMER_HDR
+#ifndef SCOPEDTIMER_H
+#define SCOPEDTIMER_H
 
 #include <string>
 #include <map>
@@ -14,8 +14,8 @@
 
 #define PROFILE(x) CScopedTimer t(std::string(#x), ai->cb);
 
-// Time interval in logic frames
-#define TIME_INTERVAL 1000
+// Time interval in logic frames (1 min)
+#define TIME_INTERVAL 1800
 
 static const float3 colors[] = {
 	float3(1.0f, 0.0f, 0.0f),
@@ -29,63 +29,27 @@ static const float3 colors[] = {
 };
 
 class CScopedTimer {
-	public:
-		CScopedTimer(const std::string& s, IAICallback *_cb): cb(_cb), task(s) {
-			initialized = true;
-			
-			if (std::find(tasks.begin(), tasks.end(), task) == tasks.end()) {
-				taskIDs[task] = tasks.size();
-#if !defined(BUILDING_AI_FOR_SPRING_0_81_2)
-				cb->DebugDrawerSetGraphLineColor(taskIDs[task], colors[taskIDs[task] % 8]);
-				cb->DebugDrawerSetGraphLineLabel(taskIDs[task], task.c_str());
-#endif
-				tasks.push_back(task);
-				
-				curTime[task] = cb->GetCurrentFrame();
-				prevTime[task] = 0;
-			}
+	
+public:
+	CScopedTimer(const std::string& s, IAICallback *_cb);
+	~CScopedTimer();
 
-			t1 = GetEngineRuntimeMSec();
-		}
+	static unsigned int GetEngineRuntimeMSec() {
+		boost::xtime t;
+		boost::xtime_get(&t, boost::TIME_UTC);
+		const unsigned int milliSeconds = t.sec * 1000 + (t.nsec / 1000000);
+		return milliSeconds;
+	}
 
-		~CScopedTimer() {
-			t2 = GetEngineRuntimeMSec();
-
-			if (!initialized)
-				return;
-#if !defined(BUILDING_AI_FOR_SPRING_0_81_2)
-			unsigned int curFrame = cb->GetCurrentFrame();
-			
-			for (size_t i = 0; i < tasks.size(); i++) {
-				const int taskID = taskIDs[tasks[i]];
-				
-				if (tasks[i] == task)
-					prevTime[task] = t2 - t1;
-				
-				cb->DebugDrawerAddGraphPoint(taskID, curFrame, prevTime[tasks[i]]);
-
-				if ((curFrame - curTime[tasks[i]]) >= TIME_INTERVAL)
-					cb->DebugDrawerDelGraphPoints(taskID, 1);
-			}
-#endif
-		}
-
-		static unsigned int GetEngineRuntimeMSec() {
-			boost::xtime t;
-			boost::xtime_get(&t, boost::TIME_UTC);
-			const unsigned int milliSeconds = t.sec * 1000 + (t.nsec / 1000000);
-			return milliSeconds;
-		}
-
-	private:
-		IAICallback *cb;
-		const std::string task;
-		unsigned int t1, t2;
-		bool initialized;
-		
-		static std::vector<std::string> tasks;
-		static std::map<std::string, int> taskIDs;
-		static std::map<std::string, unsigned int> curTime, prevTime;
+private:
+	static std::vector<std::string> tasks;
+	static std::map<std::string, int> taskIDs;
+	static std::map<std::string, unsigned int> curTime, prevTime;
+	
+	IAICallback *cb;
+	bool initialized;
+	unsigned int t1, t2;
+	const std::string task;
 };
 
 #endif
